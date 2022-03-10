cdd3ca36e052b7760bc102eedcbef36b24a32890
Fernando Soares <santosfernando2377@gmail.com>
2022-03-09 23:48:34 -0300

    Foi criado o componente input

---
+++ components/input.js
@@ -0,0 +1,8 @@
+export default function Input(props) {
+    return (
+        <div>
+          <label htmlFor={props.name} className="sr-only">{props.label}</label>
+          <input id="email-address" name={props.name} type={props.type} autoComplete={props.autoComplete} required className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ` + props.className} placeholder={props.placeholder}/>
+        </div>
+    )
+}
--- layout/login.js
+++ layout/login.js
@@ -1,3 +1,5 @@
+import Input from "../components/input"
+
 export default function Login() {
     return (
         <main>
@@ -14,14 +16,22 @@ export default function Login() {
     <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true"/>
       <div className="rounded-md shadow-sm -space-y-px">
-        <div>
-          <label htmlFor="email-address" className="sr-only">Email address</label>
-          <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
-        </div>
-        <div>
-          <label htmlFor="password" className="sr-only">Password</label>
-          <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
-        </div>
+        <Input
+        label="E-mail"
+        name="email"
+        type="email"
+        autoComplete="email"
+        placeholder="Email"
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-2"
+        />
+        <Input
+        label="Senha"
+        name="senha"
+        type="password"
+        autoComplete="password"
+        placeholder="Senha"
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-2"
+        />
       </div>
 
       <div className="flex items-center justify-between">
