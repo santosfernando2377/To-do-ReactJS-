c842143a0a0ed46abd6524e8e955013f4c254a0d
Fernando Soares <santosfernando2377@gmail.com>
2022-03-10 07:18:04 -0300

    Capturando os dados e validando se o usuário existe

--- components/input.js
+++ components/input.js
@@ -2,7 +2,15 @@ export default function Input(props) {
     return (
         <div>
           <label htmlFor={props.name} className="sr-only">{props.label}</label>
-          <input id="email-address" name={props.name} type={props.type} autoComplete={props.autoComplete} required className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ` + props.className} placeholder={props.placeholder}/>
+          <input 
+          id={props.name} 
+          name={props.name} 
+          type={props.type} 
+          autoComplete={props.autoComplete} 
+          required 
+          className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm ` + props.className} 
+          placeholder={props.placeholder}
+          onChange={props.onChange}/>
         </div>
     )
 }
